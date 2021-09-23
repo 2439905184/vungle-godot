45e0948f19d393700e78a01fb8b3e8af45970d91
2439905184 <suchenglong018@126.com>
2021-09-23 18:53:20 +0800

    初步添加广告
    初步添加广告

--- VunglePlugin/Vungle/src/main/java/com/littlesandbox/vungle/VunglePlugin.java
+++ VunglePlugin/Vungle/src/main/java/com/littlesandbox/vungle/VunglePlugin.java
@@ -6,6 +6,7 @@ import org.godotengine.godot.plugin.UsedByGodot;
 
 public class VunglePlugin extends GodotPlugin
 {
+	private String tag = this.class.toString();
     public VunglePlugin(Godot godot)
     {
         super(godot);
@@ -14,9 +15,62 @@ public class VunglePlugin extends GodotPlugin
     public String getPluginName() {
         return "VunglePlugin";
     }
+	//初始化sdk appid由vungle平台提供
     @UsedByGodot
-    public void init()
+    public void init(appId)
     {
+		Vungle.init(appId, getApplicationContext(), new InitCallback() {
+		@Override
+		public void onSuccess() {
+			Log.i(tag,"初始化成功")
+		// SDK has successfully initialized
+		}
 
+		  @Override
+		  public void onError(VungleException exception) {
+			// SDK has failed to initialize
+			Log.e(tag,"初始化失败"+exception.toString());
+		  }
+
+		  @Override
+		  public void onAutoCacheAdAvailable(String placementId) {
+			// Ad has become available to play for a cache optimized placement
+			//当广告缓存好时触发。
+		  }
+		};
     }
+	//播放奖励广告
+	@UsedByGodot
+	public void show_ad()
+	{
+		if(Vungle.isInitialized())
+		{
+
+			Vungle.loadAd("placementId",new  LoadAdCallback() {
+			@Override
+			public void onAdLoad(String placementReferenceId)
+			{ 
+				if(Vungle.canPlayAd(@NonNull String id))
+				{
+					Vungle.playAd("PLACEMENT_ID", null, new PlayAdCallback() { 
+					@Override 
+					public void onAdStart(String placementReferenceId) { } 
+					@Override
+					public void onAdEnd(String placementReferenceId, boolean completed, boolean isCTAClicked) { } 
+					@Override
+					public void onError(String placementReferenceId, VungleException exception) { } 
+				  });
+				}
+			}
+
+			@Override
+			public void onError(String placementReferenceId, VungleException exception)
+			{
+				Log.e(placementId,exception.toString());
+			}
+		  });
+		
+		}
+	}
+	
 }
